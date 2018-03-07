ed15a567b3173c969ad07fc5d7188733602c5df9
Jason Shi <jasonxshi2002@gmail.com>
2018-03-07 11:18:54 -0800

    fds

--- heap.cpp
+++ heap.cpp
@@ -4,12 +4,21 @@
 
 using namespace std;
 
+void heapify(int* stor, int input, int count){
+    
+}
+
 int main(){
+    int *stor = new int[105];
+    int count = 0;
     while(1){
+
         cout << "Enter the number you want to insert" << endl;
         int input = 0;
         cin >> input;
-        heapify(input);
+        a[count] = input;
+        heapify(stor, input, count);
+        count ++;
     }
 
 }
