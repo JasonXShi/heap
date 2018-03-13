8bee7d964530724e6c4d91f49beae1dbb3c780a1
Jason Shi <jasonxshi2002@gmail.com>
2018-03-13 12:22:15 -0700

    fds

--- heap.cpp
+++ heap.cpp
@@ -1,16 +1,22 @@
 #include <cstring>
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
 void heapify(int* stor, int input, int count){
+    int index, temp;
     
+
 }
 
 int main(){
     int *stor = new int[105];
     int count = 0;
+    ifstream file;
+    char* inputFile = new char*;
+    file.open(inputFile);
     while(1){
 
         cout << "Enter the number you want to insert" << endl;
