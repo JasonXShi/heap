5aed8e0651eb1848f1cf0c520d501529f689339c
Jason Shi <jasonxshi2002@gmail.com>
2018-03-16 04:07:21 +0100

    half

--- heap.cpp
+++ heap.cpp
@@ -4,15 +4,41 @@
 #include <fstream>
 
 using namespace std;
-
-void heapify(int* stor, int input, int count){
-    int index, temp;
-    
+void print(){
 
 }
 
+void heapify(int* stor, int input, int count){
+   	 int index, temp;
+    	temp = stor[input];
+    	index = 2*input;
+	while(index <= count){
+		if (index < count && stor[index+1] > stor[index])
+           		 index = index + 1;
+	        if (temp > stor[index])
+        	    	break;
+        	else if (temp <= stor[index])
+        	{
+            		stor[index / 2] = stor[index];
+           		 index = 2 * index;
+        	}
+    	}
+    	stor[index/2] = temp;
+    	return;
+}
+
+
+
+void build(int* stor, int count){
+	int i;
+	for(i=count/2; i>= 1; i--){
+		heapify(stor, i, count);
+	}
+}
+
+
 int main(){
-    int *stor = new int[105];
+    /*int *stor = new int[105];
     int count = 0;
     ifstream file;
     char* inputFile = new char*;
@@ -25,6 +51,21 @@ int main(){
         a[count] = input;
         heapify(stor, input, count);
         count ++;
-    }
+
+    }*/
+	char* text = new char[400];
+	cout << "Enter input as file or in console? (Type 'FILE' or 'CONSOLE')" << endl;
+	cin.getline(text, 50);
+	if(strcmp(text, "FILE")==0){
+
+	}else if(strcmp(text, "CONSOLE")==0){
+		
+	}
+
+
+
+	int count;
+	int * stor = new int[105];
+	build(stor, count);
 
 }
